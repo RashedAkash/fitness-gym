a28ad2d8ac5c3d2c6495a9a7fb430f730692ca80
hablu <131282586+hablu@users.noreply.github.com>
2023-11-24 18:41:20 +0600

    sub section responsive done

--- src/Pages/Home/NewsLetter.jsx
+++ src/Pages/Home/NewsLetter.jsx
@@ -1,7 +1,31 @@
 import React from 'react';
 import img from '../../assets/sub.jpg'
+import useAxiosPublic from '../../Hooks/useAxiosPublic';
+import Swal from 'sweetalert2'
 
 const NewsLetter = () => {
+  const axiosPublic = useAxiosPublic();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    console.log(name, email);
+    const subUser = { name, email };
+    axiosPublic.post('/subUser', subUser)
+    .then(function (response) {
+      console.log(response);
+      Swal.fire({
+  title: 'success!',
+  text: 'you are subscribe successfully',
+  icon: 'success',
+  confirmButtonText: 'Congratulation'
+})
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+  }
   return (
     <div>
       <div className="hero min-h-[80vh] bg-fixed" style={{backgroundImage: `url(${img})`}}>
@@ -10,14 +34,14 @@ const NewsLetter = () => {
     <div className="max-w-md">
      <div className="w-full max-w-md p-8 space-y-3 rounded-xl  dark:text-gray-100">
 	<h1 className="text-2xl text-[#dc1853] font-bold text-center">Subscribe</h1>
-	<form   className="space-y-6">
+	<form onSubmit={handleSubmit}  className="space-y-6">
 		<div className="space-y-1 text-sm">
 			<label  className="block dark:text-white">Name</label>
-			<input type="text" name="name" id="username" placeholder="Name" className="w-full px-4 py-3 rounded-md dark:border-white border dark:bg-gray-900 dark:text-gray-100 focus:dark:border-violet-400" />
+			<input type="text" name="name" id="username" placeholder="Name" className="w-full px-4 py-3 rounded-md dark:border-white border  dark:text-gray-900 focus:dark:border-violet-400" />
 		</div>
 		<div className="space-y-1 text-sm">
 			<label  className="block dark:text-white">Email</label>
-			<input type="email" name="email" id="email" placeholder="Email" className="w-full px-4 py-3 rounded-md border-white dark:bg-gray-900 dark:text-gray-100 focus:dark:border-violet-400" />
+			<input type="email" name="email" id="email" placeholder="Email" className="w-full px-4 py-3 rounded-md border-white  dark:text-gray-900 focus:dark:border-violet-400" />
 			
 		</div>
 		<button type='submit' className="block w-full p-3 text-center rounded-sm  dark:bg-[#dc1853] text-white">Subscribe</button>
