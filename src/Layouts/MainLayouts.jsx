bbd362ae92672688a641abb9053376af25473474
hablu <131282586+hablu@users.noreply.github.com>
2023-11-26 07:10:46 +0600

    helmet create in page title

--- src/Layouts/MainLayouts.jsx
+++ src/Layouts/MainLayouts.jsx
@@ -1,9 +1,17 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { Outlet, useLocation } from 'react-router-dom';
 import Navbar from '../Shared/Navbar/Navbar';
 import Footer from '../Shared/Footer/Footer';
 
 const MainLayouts = () => {
+  const location = useLocation();
+  useEffect(() => {
+    if (location.pathname === '/') {
+      document.title= 'Fitness | Home'
+    } else {
+      document.title = `Fitness ${location.pathname.replace('/',(' | '))}`
+    }
+  },[location.pathname])
   return (
     <div className=' overflow-hidden'>
       <Navbar />
