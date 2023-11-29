af01682f95c56d8fa8a9b639429604c76dba41fd
hablu <131282586+hablu@users.noreply.github.com>
2023-11-29 18:07:17 +0600

    today activity page create and responsive complete

--- src/Pages/Dashboard/ActivityLog.jsx
+++ src/Pages/Dashboard/ActivityLog.jsx
@@ -2,8 +2,16 @@ import React from 'react';
 
 const ActivityLog = () => {
   return (
-    <div>
-      aa
+    <div className=' '>
+      <div className="card  bg-primary text-primary-content">
+  <div className="card-body text-left">
+    <h2 className="card-title">Today's Activity : Strength training</h2>
+    <p>Your Trainer : Akash Khan</p>
+    <p>Your Training Time : 10.00 - 12.00 </p>
+    <p>Please Join this training in time. </p>
+    
+  </div>
+</div>
     </div>
   );
 };
